ff61d76adfb3f5b0e94a752d1353da1f9a5ad382
CodeHntr <paveltrokhanovsky@gmail.com>
2022-09-07 15:12:29 +0300

    finally

--- randomArray.php
+++ randomArray.php
@@ -9,7 +9,7 @@ $index = 0;
 $min = 0;
 $max = 0;
 $i = 0;
-
+$steps = 0;
 
 while (!$count) {
     echo "Введіть к-сть елементів масиву, число має бути > 10000. Введіть число:";
@@ -52,12 +52,44 @@ if (!$number) {
 
 function linearSearch($array, $number)
 {
+    $steps = 0;
     foreach ($array as $key => $value) {
+        $steps++;
         if ($value == $number) {
-            return $key;
+            return [
+                'index' => $key,
+                'steps' => $steps
+            ];
         }
     }
     return false;
+}
+
+function binarySearch($array, $number)
+{
+    sort($array);
+    $left = 0;
+    $right = count($array) - 1;
+    $result = false;
+    $steps = 0;
+
+
+    while ($left < $right && !$result) {
+        $m = (int) round(($left + $right) / 2);
+        $steps++;
+
+        if ($array[$m] == $number) {
+            $result = true;
+            echo "\n Value: $array[$m]  Знайденно! під індексом: $m . Зроблено: $steps кроків. ";
+        } elseif ($array[$m] < $number) {
+            $left = $m + 1;
+        } else {
+            $right = $m - 1;
+        }
+    }
+    if (!$result) {
+        echo "\n number $number Не знайдено. Зроблено: $steps кроків. ";
+    }
 }
 
 
@@ -91,48 +123,30 @@ function linearSearch($array, $number)
 //}
 
 
-function binarySearch(array $array, $number)
-{
-    asort($array);
-    $start = array_key_first($array);
-    $end = array_key_last($array);
+//echo "Лінійний пошук  ";
+//$startTime = microtime(true);
+//$result = linearSearch($array, $number);
+//echo "Результат лінійного пошуку: " . "\n";
+//if ($result) {
+//    echo "Число: " . $number . " Знайдене, іднекс у масиві: " . $result['index'] .  " Зроблено кроків:" . $result['steps'] . "\n";
+//} else {
+//    echo "Число не знайдено \n";
+//}
+//$time = round(microtime(true) - $startTime, 6);
+//echo "Час роботи функції 'linearSearch': " . $time . "\n";
 
-    if ($start > $end) {
-        throw new LogicException("Item not found");
-    }
 
-    $halfIndex = ($start + $end) / 2;
-
-    if ($array[$halfIndex] !== $number) {
-        if ($array[$halfIndex] < $number) {
-            $start = $halfIndex + 1;
-        } else {
-            $end = $halfIndex - 1;
-        }
-
-        return binarySearch($array, $number);
-    }
-    return $halfIndex;
-}
-
-echo "Лінійний пошук  ";
+echo "Бінарний пошук  ";
 $startTime = microtime(true);
-$result = linearSearch($array, $number);
-echo "Результат лінійного пошуку: " . "\n";
-if ($result) {
-    echo "Число: " . $number . " Знайдене, іднекс у масиві: " . $result . "\n";
-} else {
-    echo "Число не знайдено \n";
-}
+binarySearch($array, $number);
 $time = round(microtime(true) - $startTime, 6);
 echo "Час роботи функції 'linearSearch': " . $time . "\n";
+
+
+
 
 //$start = microtime(true);
 //maxMinSearch($array);
 //$time = round(microtime(true) - $start, 4);
 //echo "Час роботи функції 'maxMinSearch': " . $time . "<\n>";
-
-//echo "<br>" . binarySearch($array, $number);
-
-
 
